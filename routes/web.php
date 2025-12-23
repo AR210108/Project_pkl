5d9bf2317aa22b45291126253a9f609e8fed9489
Mutiara1945 <permanamutiara2@gmail.com>
2025-12-23 09:12:04 +0700

    update invoice & kwitansi view

--- routes/web.php
+++ routes/web.php
@@ -242,7 +242,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/data', fn() => view('finance/data_layanan'));
     Route::get('/pembayaran', fn() => view('finance/data_pembayaran'));
     Route::get('/data_in_out', fn() => view('finance/data_in_out'));
-    
+    Route::get('/invoice', fn() => view('finance/invoice'));
+    Route::get('/kwitansi', fn() => view('finance/kwitansi'));
+
     // Manager Divisi
     Route::get('/manager_divisi', fn() => view('manager_divisi/home'));
     Route::get('/pengelola_tugas', fn() => view('manager_divisi/pengelola_tugas'));
