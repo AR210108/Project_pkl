8a52d26964bf812199c86692575bc1f5f0576ab9
kubil098 <abrielpratama6@gmail.com>
2026-01-06 16:16:16 +0700

    tugas

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\PengumumanController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\SuratKerjasamaController;
 use App\Http\Controllers\PelayananController;
+use App\Http\Controllers\TugasController;
 use App\Models\Invoice;
 
 /*
@@ -365,9 +366,7 @@ Route::get('/layanan', [PelayananController::class, 'index']);
 Route::get('/kelola_order', function () {
     return view('general_manajer/kelola_order');
 });
-Route::get('/kelola_tugas', function () {
-    return view('general_manajer/kelola_tugas');
-});
+Route::get('/kelola_tugas', [TugasController::class, 'index'])->name('tugas.page');
 Route::get('/kelola_absen', function () {
     return view('general_manajer/kelola_absen');
 });
@@ -376,3 +375,11 @@ Route::get('/kelola_absen', function () {
 Route::post('/layanan', [PelayananController::class, 'store']);
 Route::put('/layanan/{id}', [PelayananController::class, 'update']);
 Route::delete('/layanan/{id}', [PelayananController::class, 'destroy']);
+
+Route::prefix('kelola_tugas')->name('tugas.')->group(function () {
+    Route::get('/data', [TugasController::class, 'getData'])->name('data');
+    Route::post('/', [TugasController::class, 'store'])->name('store');
+    Route::get('/{id}', [TugasController::class, 'show'])->name('show');
+    Route::put('/{id}', [TugasController::class, 'update'])->name('update');
+    Route::delete('/{id}', [TugasController::class, 'destroy'])->name('destroy');
+});
